8ceb20e94b13a09e67df8946f165f01a3268cf3f
Woohyuk0813 <wh081312@gmail.com>
2026-04-27 17:51:17 +0900

    수정

--- src/main/java/com/example/LunchGo/chatbot/service/NcpChatbotService.java
+++ src/main/java/com/example/LunchGo/chatbot/service/NcpChatbotService.java
@@ -113,6 +113,7 @@ public class NcpChatbotService {
         }
     }
 
+
     private String makeSignature(String message, String secretKey) {
         try {
             SecretKeySpec signingKey = new SecretKeySpec(
